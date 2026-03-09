a7b0f0ecfa893f392f6b2aa06594a6d3740f58b6
LechiHound69 <cristobalmejiam@gmail.com>
2026-03-09 10:14:41 -0500

    fix: esoooo

---
+++ src/main.py
@@ -0,0 +1,113 @@
+from database import SessionLocal, engine, Base
+from models import Usuario, Nota
+
+print("Creando tablas en Neon uwu")
+# Base.metadata.create_all(bind=engine)
+print("Tabla lista UwU")
+
+db = SessionLocal()
+
+
+def crear():
+    print("--- 1. Metiendo xd ---")
+    lechi = Usuario(nombre="Lechi Master", email="shawrigoberto@gmail.com", edad=19)
+
+    nota1 = Nota(titulo="Recordatorio", contenido="Recuerda seguir siendo el mejor :)")
+    nota2 = Nota(titulo="Obras maestras", contenido="Beastars, JJBA, HxH")
+
+    lechi.notas.append(nota1)
+    lechi.notas.append(nota2)
+
+    db.add(lechi)
+
+    db.commit()
+
+    db.refresh(lechi)
+
+    print("Usuario y notas creadas OwO: {lechi}")
+
+
+def consultar():
+    print("\n--- 2. Consultando xd ---")
+    usuario_encontrado = (
+        db.query(Usuario).filter(Usuario.nombre == "Lechi Master").first()
+    )
+    print(f"Lo encontré en la DB OwO: {usuario_encontrado.nombre}")
+    print("Sus notas guardadas son:")
+    for nota in usuario_encontrado.notas:
+        print(f" - {nota.titulo}: {nota.contenido}")
+    return usuario_encontrado
+
+
+def actualizar(usuario_encontrado):
+    print("\n--- 3. Actualizando xd ---")
+    usuario_encontrado.edad = 20
+    usuario_encontrado.nombre = "Lechi God UwU"
+
+    db.commit()
+    print("Usuario actualizado :3")
+
+
+def borrar(usuario_encontrado):
+    print("\n--- 4. BORRANDO ---")
+    db.delete(usuario_encontrado)
+    db.commit()
+    print("za hando gakesu")
+
+    check = db.query(Usuario).filter(Usuario.nombre == "Lechi God UwU").first()
+    print(f"Sigues existiendo?: {check}")
+
+    db.rollback()
+
+
+def actualizar_notas():
+    nota1 = (
+        db.query(Nota)
+        .filter(Nota.id == 1 and Nota.usuario_id == usuario_encontrado.id)
+        .first()
+    )
+    nota1.titulo = "Recordatorio diario :3"
+
+    nota2 = (
+        db.query(Nota)
+        .filter(Nota.id == 2, Nota.usuario_id == usuario_encontrado.id)
+        .first()
+    )
+    nota2.contenido = "Beastars, JJBA, HxH, Pokémon (Los juegos)"
+
+    db.commit()
+
+
+usuario_encontrado = None
+
+while True:
+    print(
+        "Ingrese una opción xdxdxddd:\n1. Crear usuario\n2. Consultar usuario\n3. Actualizar usuario\n4. Borrar usuario\n5. Actualizar notas\n6. Salir"
+    )
+    opcion = input()
+    match opcion:
+        case "1":
+            crear()
+        case "2":
+            usuario_encontrado = consultar()
+        case "3":
+            if usuario_encontrado == None:
+                print("Aún no hay ningún usuario encontrado para actualizar UnU")
+            else:
+                actualizar(usuario_encontrado)
+        case "4":
+            if usuario_encontrado == None:
+                print("Aún no hay ningún usuario encontrado para borrar UnU")
+            else:
+                borrar(usuario_encontrado)
+        case "5":
+            if usuario_encontrado == None:
+                print("Aún no hay ningún usuario para buscar notas UnU")
+            else:
+                actualizar_notas()
+        case "6":
+            db.close()
+            print("Saliendo TwT")
+            break
+        case _:
+            print("Opción incorrecta UnU, saliendo")
---
+++ src/models.py
@@ -0,0 +1,40 @@
+import uuid
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Usuario(Base):
+
+    __tablename__ = "usuariosuwu"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    nombre = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True)
+    telefono = Column(String, nullable=True)
+    edad = Column(Integer, nullable=True)
+
+    notas = relationship(
+        "Nota",
+        back_populates="propietario",
+        cascade="all, delete-orphan",
+    )
+
+    def __repr__(self):
+        return f"<Usuario(id={self.id}, nombre='{self.nombre}', email='{self.email}', edad={self.edad})>"
+
+
+class Nota(Base):
+    __tablename__ = "notasowo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String, index=True)
+    contenido = Column(String)
+
+    usuario_id = Column(UUID(as_uuid=True), ForeignKey("usuariosuwu.id"))
+
+    propietario = relationship("Usuario", back_populates="notas")
+
+    def __repr__(self):
+        return f"<Nota '{self.titulo}'>"
